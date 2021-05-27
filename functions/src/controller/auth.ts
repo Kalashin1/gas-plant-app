8f13fe85158bd1003c98db977b889948b120fb02
kinanee Samson <kinaneeSamsonJohn@gmail.com>
2021-05-27 14:28:24 +0100

    cashier name now showing in dashboard, route gaurds working properly

--- functions/src/controller/auth.ts
+++ functions/src/controller/auth.ts
@@ -1,70 +1,106 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from "express";
 // import { Admin } from '../interface/Admin'
-import { auth, db } from '../firebase-settings'
+import { auth, db } from "../firebase-settings";
 
 // * Sign the user up with firebase auth
 export const signupUser = async (req: Request, res: Response) => {
-  const { email, password, adminStatus } = req.body
- 
-  try{
-    const { user } = await auth.createUserWithEmailAndPassword(email, password)
-    await db.collection('user').doc(user.uid).set({email, adminStatus})
-    res.status(200).end()
+  const { email, password, adminStatus } = req.body;
+
+  try {
+    const { user } = await auth.createUserWithEmailAndPassword(email, password);
+    await db.collection("user").doc(user.uid).set({ email, adminStatus });
+    res.status(200).json({ message: "success" });
+  } catch (err) {
+    res.status(400).json({ errorCode: err.code, errorMessage: err.message });
   }
-  catch (err) {
-    res.status(400).json({ errorCode: err.code, errorMessage: err.message})
-  }
-}
+};
 
 // * Log the user in with firebase auth
 export const loginUser = async (req: Request, res: Response) => {
-  const { email, password } = req.body
-  
-  try{
-    await auth.signInWithEmailAndPassword(email, password)
-    res.status(200).end()
+  const { email, password } = req.body;
+
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+    res.status(200).end();
+  } catch (err) {
+    res.status(400).json({ errorCode: err.code, errorMessage: err.message });
   }
-  catch (err) {
-    res.status(400).json({ errorCode: err.code, errorMessage: err.message})
-  }
-}
+};
 
 // * Sign the user out
 export const signOut = async (req: Request, res: Response) => {
-
   try {
-    await auth.signOut()
-    res.status(200).redirect('/login')
+    await auth.signOut();
+    res.status(200).redirect("/login");
   } catch (err) {
-    res.status(400).json({ errorCode: err.code, errMessage: err.message})
+    res.status(400).json({ errorCode: err.code, errMessage: err.message });
   }
-}
+};
 
 //  * Get the current logged in user
-export const isUserLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
-  const user = auth.currentUser
+export const isUserLoggedIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = auth.currentUser;
 
-  if (user !== null || undefined ) {
-    next()
+  if (user !== null || undefined) {
+    next();
   } else {
-    res.redirect('/login')
+    res.redirect("/login");
   }
 
   // next()
-}
+};
 
 // * Check if the user is and admin
-export const isUserAdmin = async (req: Request, res: Response, next: NextFunction) => {
-  const user = auth.currentUser
-
+export const isUserAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = auth.currentUser;
   if (user) {
-    const docRef = await db.collection('users').doc(user.uid).get()
-    const userDoc = docRef.data()
+    const userRef = await db
+      .collection("users")
+      .where("email", "==", user.email)
+      .limit(1)
+      .get();
 
-    userDoc.isAdmin ? next() : res.redirect('/dashboard/index')
+    let doc: any = {};
+
+    userRef.forEach((_doc) => (doc = _doc.data()));
+
+    doc.adminStatus ?? next();
+    res.redirect("/dashboard/index");
+    next();
   } else {
-    res.redirect('/login')
+    res.redirect("/login");
   }
 
   // next()
-}
+};
+
+export const getLoggedInUser = async (req: Request, res: Response) => {
+  const user = auth.currentUser;
+
+  if (user) {
+    const userRef = await db.collection("users").doc(user.uid).get();
+
+    const userDoc = userRef.data();
+
+    let _userDoc;
+
+    const staffRef = await db
+      .collection("staffs")
+      .where("email", "==", user.email)
+      .limit(1)
+      .get();
+
+    staffRef.forEach((staff) => {
+      _userDoc = staff.data();
+    });
+    res.json({ _userDoc });
+  }
+};
--- functions/src/router/router.ts
+++ functions/src/router/router.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import { Request, Response, Router } from "express";
 
 //  * Auth functions
 import {
@@ -7,6 +7,7 @@ import {
   signOut,
   isUserLoggedIn,
   isUserAdmin,
+  getLoggedInUser,
 } from "../controller/auth";
 
 //  * Product Controller
@@ -16,7 +17,6 @@ import {
   fetchProductInfo,
   setGasPrice,
   getGasPrice,
-  
   fetchProductPage,
   addNewProduct,
   getAllProducts,
@@ -27,42 +27,55 @@ import {
 } from "../controller/product-cont";
 
 // * Customer functions
-import { addNewCustomer, deleteCustomer, editCustomer, getAllCustomers, getCustomer, renderEditCustomer } from "../controller/customer";
+import {
+  addNewCustomer,
+  deleteCustomer,
+  editCustomer,
+  getAllCustomers,
+  getCustomer,
+  renderEditCustomer,
+} from "../controller/customer";
 
 // * Staff functions
-import { addStaff, deleteStaff, editStaff, getAllStaffs, getStaff, renderEditStaffPage, renderStaffPage } from "../controller/staff";
+import {
+  addStaff,
+  deleteStaff,
+  editStaff,
+  getAllStaffs,
+  getStaff,
+  renderEditStaffPage,
+  renderStaffPage,
+} from "../controller/staff";
 
 // * Expenses functions
 import {
   renderExpensesPage,
   makeExpenses,
   fetchAllExpenses,
-} from '../controller/expenses'
+} from "../controller/expenses";
 
 // * Sales functions
-import {
-  renderSalesPage, 
-  makeSales,
-  getAllSales,
-} from '../controller/sales'
+import { renderSalesPage, makeSales, getAllSales } from "../controller/sales";
 import { addSettings, renderSettingsPage } from "../controller/settings";
 
-const router = express.Router();
+const router = Router();
 
 // *  returns the home page
-router.get("/home", (req: express.Request, res: express.Response) => {
+router.get("/home", (req: Request, res: Response) => {
   res.render("index");
 });
 
-
 //  * renders the home page on the dashboard
-router.get("/dashboard/index", isUserLoggedIn, (req: express.Request, res: express.Response) => {
+router.get(
+  "/dashboard/index",
+  isUserLoggedIn,
+  (req: Request, res: Response) => {
     res.render("dashboard/index");
   }
 );
 
 //  * renders the signup page
-router.get("/signup", (req: express.Request, res: express.Response) => {
+router.get("/signup", (req: Request, res: Response) => {
   res.render("signup");
 });
 
@@ -70,9 +83,17 @@ router.get("/signup", (req: express.Request, res: express.Response) => {
 router.post("/signup", signupUser);
 
 //  * renders the login page
-router.get("/login", (_req: express.Request, res: express.Response) => {
+router.get("/login", (_req: Request, res: Response) => {
   res.render("login");
 });
+
+// * Make a User a chashier
+router.get("/make-cashier", isUserAdmin, (_req: Request, res: Response) => {
+  res.render("edit/make-cashier");
+});
+
+// * Get the current Logged In User
+router.get("/user", getLoggedInUser);
 
 //  * logs the user in
 router.post("/login", loginUser);
@@ -80,122 +101,105 @@ router.post("/login", loginUser);
 //  * logs the user out
 router.get("/signout", signOut);
 
-
-
-
 // * SETTINGS
 // * render the settings page
-router.get('/settings', isUserAdmin, renderSettingsPage)
+router.get("/settings", isUserAdmin, renderSettingsPage);
 // * upload the settings
-router.post('/settings', isUserAdmin, addSettings)
-
-
-
-
-
-
+router.post("/settings", isUserAdmin, addSettings);
 
 //  * get the product page
-router.get('/dashboard/products', isUserLoggedIn, fetchProductPage)
+router.get("/dashboard/products", isUserLoggedIn, fetchProductPage);
 
 //  * creates a new product
-router.post('/product/add', isUserLoggedIn, addNewProduct)
+router.post("/product/add", isUserLoggedIn, addNewProduct);
 
 // * get a single product
-router.get('/product/id/:id', isUserLoggedIn, getProduct)
+router.get("/product/id/:id", isUserLoggedIn, getProduct);
 
 // * gets all the products
-router.get('/products/all', isUserLoggedIn, getAllProducts)
+router.get("/products/all", isUserLoggedIn, getAllProducts);
 // * renders the edit product page
-router.get('/product/edit/:id', isUserLoggedIn,renderEditProdcutPage)
+router.get("/product/edit/:id", isUserLoggedIn, renderEditProdcutPage);
 // * edit the post
-router.post('/product/edit/:id', isUserLoggedIn, editProduct)
+router.post("/product/edit/:id", isUserAdmin, editProduct);
 // * delete a product
-router.get('/product/delete/:id', isUserLoggedIn, deleteProduct)
-
-
-
+router.get("/product/delete/:id", isUserAdmin, deleteProduct);
 
 //  * renders the entry pages
 router.get("/dashboard/entry", isUserLoggedIn, renderEntry);
 
 //  * Makes a new gas entry
-router.post("/entry", isUserLoggedIn, makeGasEntry);
+router.post("/entry", isUserAdmin, makeGasEntry);
 
 // *  get the latest product info
 router.get("/product/info", isUserLoggedIn, fetchProductInfo);
 
 // *  renders the gas price page
-router.get('/dashboard/price', isUserLoggedIn, (req: express.Request, res: express.Response) => {
-  res.render('dashboard/gas-price')
-})
+router.get(
+  "/dashboard/price",
+  isUserLoggedIn,
+  (req: Request, res: Response) => {
+    res.render("dashboard/gas-price");
+  }
+);
 
 // *  set the gas price
-router.post('/gas/price', isUserLoggedIn, setGasPrice)
+router.post("/gas/price", isUserLoggedIn, setGasPrice);
 
 //  * get the gas price page
-router.get('/gas/price', isUserLoggedIn, getGasPrice)
-
-
+router.get("/gas/price", isUserLoggedIn, getGasPrice);
 
 //  * renders the customers page
-router.get('/dashboard/customers', isUserLoggedIn, (req: express.Request, res: express.Response) => {
-  res.render('dashboard/customers')
-})
+router.get(
+  "/dashboard/customers",
+  isUserLoggedIn,
+  (req: Request, res: Response) => {
+    res.render("dashboard/customers");
+  }
+);
 
 // *  create a new customer
-router.post('/customer', isUserLoggedIn, addNewCustomer)
+router.post("/customer", isUserLoggedIn, addNewCustomer);
 //  * retrieve all the customers
-router.get('/customers/all', isUserLoggedIn, getAllCustomers)
-// * get a single customer 
-router.get('/customer/id/:id', isUserLoggedIn, getCustomer)
+router.get("/customers/all", isUserLoggedIn, getAllCustomers);
+// * get a single customer
+router.get("/customer/id/:id", isUserLoggedIn, getCustomer);
 // * render edit customer page
-router.get('/customer/edit/:id', isUserLoggedIn, renderEditCustomer)
+router.get("/customer/edit/:id", isUserLoggedIn, renderEditCustomer);
 // * edit a single customer
-router.post('/customer/edit/:id', isUserLoggedIn, editCustomer)
+router.post("/customer/edit/:id", isUserLoggedIn, editCustomer);
 // * delete a customer
-router.get('/customer/delete/:id', isUserLoggedIn, deleteCustomer)
-
-
-
-
-
-
-
-
+router.get("/customer/delete/:id", isUserAdmin, deleteCustomer);
 
 //  * create a staff
-router.post('/staff/create', isUserLoggedIn, addStaff)
+router.post("/staff/create", isUserAdmin, addStaff);
 
 // * render the staff page
-router.get('/dashboard/staffs', isUserLoggedIn, renderStaffPage)
+router.get("/dashboard/staffs", isUserLoggedIn, renderStaffPage);
 // * Gets all the staffs
-router.get('/staff/all', isUserLoggedIn, getAllStaffs)
-// * Gets all the 
-router.get('/staff/id/:id', isUserAdmin, getStaff)
+router.get("/staff/all", isUserLoggedIn, getAllStaffs);
+// * Gets the staff with a particular id
+router.get("/staff/id/:id", isUserLoggedIn, getStaff);
 // * render edit staff page
-router.get('/staff/edit/:id', isUserAdmin, renderEditStaffPage)
+router.get("/staff/edit/:id", isUserLoggedIn, renderEditStaffPage);
 // * edit the staff
-router.post('/staff/edit/:id', isUserAdmin, editStaff)
+router.post("/staff/edit/:id", isUserAdmin, editStaff);
 // * delete a staff
-router.get('/staff/delete/:id', isUserAdmin, deleteStaff)
-
-
+router.get("/staff/delete/:id", isUserAdmin, deleteStaff);
 
 //  * EXPENSES
 //  * render expenses page
-router.get('/dashboard/expenses', isUserLoggedIn, renderExpensesPage)
+router.get("/dashboard/expenses", isUserLoggedIn, renderExpensesPage);
 // * Make a new expenses
-router.post('/expenses/new', isUserLoggedIn, makeExpenses)
+router.post("/expenses/new", isUserLoggedIn, makeExpenses);
 // * retrieve all the expenses
-router.get('/expenses/all', isUserLoggedIn, fetchAllExpenses)
-
+router.get("/expenses/all", isUserLoggedIn, fetchAllExpenses);
 
 //  * SALES
 // * render sales page
-router.get('/dashboard/sales', isUserLoggedIn, renderSalesPage)
+router.get("/dashboard/sales", isUserLoggedIn, renderSalesPage);
 // * make a sale
-router.post('/sales/new', isUserLoggedIn, makeSales)
+router.post("/sales/new", isUserLoggedIn, makeSales);
 // * retrieve all sales
-router.get('/sales/all', isUserLoggedIn, getAllSales)
+router.get("/sales/all", isUserLoggedIn, getAllSales);
 export { router };
